3eba8a8d61bdce1f5182c77d3d3fa84f602ca222
Chance Smith <chancesmith97@hotmail.com>
2020-08-05 19:14:55 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-  validates :name, presence: true 
-  validates :email, uniqueness: true 
+  validates :name, presence: true
+  validates :email, uniqueness: true
   validates :phone_number, numericality: { equal_to: 10 }
 end
